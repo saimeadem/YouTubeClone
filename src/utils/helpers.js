ccc0e0e99d17a05e6dab3e8e901cc705fbe84ad3
Saime <132603290+saimeadem@users.noreply.github.com>
2023-10-30 13:00:20 +0300

    Update helpers.js

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -7,8 +7,7 @@ const options = {
         lang: 'tr',
     },
     headers: {
-        'X-RapidAPI-Key': '5914ca3586mshd0138071e7f0dc0p1aab19jsn7b05f9ad4ba7',
-        'X-RapidAPI-Host': 'yt-api.p.rapidapi.com',
+        ----your APİ----
     },
 };
 
